07146c91c0cc8db7d2078f82abf2e29217cea60e
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-13 10:31:53 +0200

    AsyncTask class finished.

--- app/src/main/java/com/example/downloadimageapplication/DownloadImage.java
+++ app/src/main/java/com/example/downloadimageapplication/DownloadImage.java
@@ -1,13 +1,48 @@
 package com.example.downloadimageapplication;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+    private MainActivity pantallaImagen;
+
+    public DownloadImage(MainActivity mainActivity) {
+        this.pantallaImagen = mainActivity;
+    }
 
     @Override
     protected Bitmap doInBackground(String... strings) {
         Bitmap bitmap = null;
+        InputStream in = null;
+        int respuesta = -1;
+        URL url = null;
+        HttpURLConnection httpConn = null;
+
+        Log.d(getClass().getCanonicalName(), "Iniciando la descarga.");
+        try {
+            Log.d("MIAPP", "Descargando . . .");
+            url = new URL(strings[0]);
+            httpConn = (HttpURLConnection) url.openConnection();
+
+            respuesta = httpConn.getResponseCode();
+            if (respuesta == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+                bitmap = BitmapFactory.decodeStream(in);
+                in.close();
+            }
+            Log.d(getClass().getCanonicalName(), "Descarga terminada.");
+
+        } catch (Exception e) {
+            // Auto-generated catch block
+            Log.d(getClass().getCanonicalName(), "ERROR descargando la imagen", e);
+        }
 
         return bitmap;
     }
@@ -15,10 +50,14 @@ public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        Log.d(getClass().getCanonicalName(), "Antes de iniciar la descarga.");
     }
 
     @Override
     protected void onPostExecute(Bitmap bitmap) {
         super.onPostExecute(bitmap);
+        Log.d(getClass().getCanonicalName(), "Terminada la descarga.");
+
+        this.pantallaImagen.mostrarImagenDescargada(bitmap);
     }
 }
--- app/src/main/java/com/example/downloadimageapplication/MainActivity.java
+++ app/src/main/java/com/example/downloadimageapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.downloadimageapplication;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DownloadImage di;
+    private static final String URL_IMAGEN = "https://en.wikipedia.org/wiki/Lion#/media/File:Lion_waiting_in_Namibia.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.di = new DownloadImage(this);
+        di.execute(URL_IMAGEN);
+    }
+
+    public void mostrarImagenDescargada(Bitmap bitmap) {
+        // Mostrar la imagen o mensaje de Error.
+        if (null == bitmap) {
+            Toast.makeText(this, R.string.error_mainactivity_no_download, Toast.LENGTH_LONG).show();
+        } else {
+            ImageView iv = findViewById(R.id.downloadedImage);
+            iv.setImageBitmap(bitmap);
+        }
     }
 }
