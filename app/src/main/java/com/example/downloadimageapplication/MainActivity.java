3368c72d4007e3b0907c52b17e993e9728b59dae
Fernando López Carballeda <flc.2056@zohomail.eu>
2019-06-13 11:22:40 +0200

    Añadida la ProgressBar.

--- app/src/main/java/com/example/downloadimageapplication/MainActivity.java
+++ app/src/main/java/com/example/downloadimageapplication/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.downloadimageapplication;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar pb;
     private DownloadImage di;
     // Image from https://all-free-download.com/free-photos/download/male-lion_189768.html
     private static final String URL_IMAGEN = "https://images.all-free-download.com/images/graphiclarge/male_lion_189768.jpg";
@@ -17,11 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        pb = findViewById(R.id.progress_circular);
+        pb.setVisibility(View.VISIBLE);
+
         this.di = new DownloadImage(this);
         di.execute(URL_IMAGEN);
     }
 
     public void mostrarImagenDescargada(Bitmap bitmap) {
+        pb.setVisibility(View.GONE);
+
         // Mostrar la imagen o mensaje de Error.
         if (null == bitmap) {
             Toast.makeText(this, R.string.error_mainactivity_no_download, Toast.LENGTH_LONG).show();
